e57e015a7b62f6efa5e83133a0522f283db545b8
yilberth <yiarpe99@gmail.com>
2025-06-10 04:13:02 -0500

    fix: Eliminar la línea de importación de DB en start.sh para limpiar el script de inicio

--- hoteles-backend/docker/start.sh
+++ hoteles-backend/docker/start.sh
@@ -3,9 +3,6 @@ set -e
 
 cd /app
 
-# Arreglar el import de DB
-sed -i '3a use Illuminate\Support\Facades\DB;' routes/api.php
-
 php artisan config:clear
 php artisan route:cache
 php artisan migrate --force || true
